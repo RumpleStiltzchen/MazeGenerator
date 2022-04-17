af8ec3e271b805bed930898bfac21d661df62c0b
RumpleStiltzchen <64101489+RumpleStiltzchen@users.noreply.github.com>
2022-04-16 21:32:21 -0300

    Update Cell.js

--- Cell.js
+++ Cell.js
@@ -21,15 +21,30 @@ function Cell(x, y)
         var y = this.y*w;
         stroke(0);
         
+    
         if(this.walls[0])
+          {
+            
+          
           line(x, y, x+w, y); //top
+          }
         if(this.walls[3])
+          {
+            
+  
           line(x, y+w, x, y); // right
+          }
         if (this.walls[2])
-          line(x+w, y+w, x, y+w); // bottom
-        if(this.walls[1])
-           line(x+w, y, x+w, y+w); // left
+          {
+            
 
+          line(x+w, y+w, x, y+w); // bottom
+          }
+        if(this.walls[1])
+          {
+           line(x+w, y, x+w, y+w); // left
+          }
+        
         if(this.visited)
         {
           noStroke();
@@ -75,4 +90,41 @@ function Cell(x, y)
         }
     
   }
+    this.checkNeighborsVisited = function()
+  {
+    var neighbors = [];
+    
+    var top = grid[index(x, y-1)];
+    var bottom = grid[index(x, y+1)];
+    var right = grid[index(x + 1, y)];
+    var left = grid[index(x - 1, y)];
+    
+    if(top && top.visited)
+      {
+        neighbors.push(top);
+      }
+    if(left && left.visited)
+      {
+        neighbors.push(left);
+      }
+    if(bottom && bottom.visited)
+      {
+        neighbors.push(bottom);
+      }
+    if(right && right.visited)
+      {
+        neighbors.push(right);
+      }
+    
+    if ( neighbors.length > 0 )
+      {
+        var r = floor(random(0, neighbors.length));
+        return neighbors[r];
+      }else
+        {
+          return undefined;
+        }
+    
+  }
+  
 }
